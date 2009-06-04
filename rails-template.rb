aff2fa65b732e7e9fdfd7da74e9b06e28b9f5561
Mark Van Holstyn <mvanholstyn@gmail.com>
2009-06-04 14:09:19 -0400

    Adding capify call to the template. Added a TODO for the future as well as some resources for future additions.

--- rails-template.rb
+++ rails-template.rb
@@ -29,6 +29,8 @@ test:
 END
 run "cp config/database.yml.example config/database.yml"
 
+run "capify ."
+
 # TODO: Can we make this pull down the latest release instead of master? Maybe we should add them as gems? (some need to be in environments/test.rb)
 plugin 'rspec', :git => 'git://github.com/dchelimsky/rspec.git'
 plugin 'rspec-rails', :git => 'git://github.com/dchelimsky/rspec-rails.git'
@@ -59,5 +61,61 @@ file '.gitignore', <<-END
 /log
 /tmp
 END
+# TODO: Maybe this can add the repository as a remote if it doesn't already exist?
 git :add => '.'
 git :commit => "-am 'Initial Commit'"
+
+__END__
+
+Here are some other resources and examples of things this can do
+
+# http://github.com/jeremymcanally/rails-templates/tree/master/
+# http://gist.github.com/33337
+# http://github.com/imajes/rails-template/tree/master
+# http://ramblingsonrails.com/how-to-use-the-new-templates-in-rails
+# http://github.com/ryanb/rails-templates/tree/master
+
+if yes?("Freeze rails gems?")
+  # do something
+end
+
+route "map.login '/login', :controller => 'sessions', :action => 'new'"
+
+rakefile("cruise_controle.rake") do
+  <<-TASK
+    desc "Run all the tests, including API and acceptance tests"
+    task :cruise do
+      Rake::Task['db:migrate'].invoke
+      Rake::Task['spec'].invoke
+      Rake::Task['spec:stories'].invoke
+      Rake::Task['metrics:all'].invoke
+      Rake::Task['flogger:record'].invoke
+    end
+  TASK
+end
+
+task :thing do
+  # blah
+end
+
+initializer 'form_builder.rb', <<-CODE
+  ActionView::Base.default_form_builder = SemanticFormBuilder
+CODE
+
+rake "db:migrate", :env => 'production'
+
+lib lib_name, <<-CODE
+  class Shiny
+  end
+CODE
+
+inside('vendor') { 
+  # do stuff inside the vendor folder
+}
+
+plugin 'rspec', :git => 'git://github.com/dchelimsky/rspec.git', :submodule => true
+gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com'
+gem "haml", :git => "git://github.com/nex3/haml.git"
+rake "gems:install", :sudo => true
+
+git :submodule => "init"
