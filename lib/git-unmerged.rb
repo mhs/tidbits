c94197ef5fbde6a3f32cb46cd31a919e2f49362b
Zach Dennis <zach.dennis@gmail.com>
2015-10-01 14:11:26 -0400

    Adding an --only option to git-unmerged to only compare the given branches against the upstream.
    E.g.
         # to see if we have commits in release-candidate not already in acceptance
         git unmerged --upstream origin/acceptance --only release-candidate

--- lib/git-unmerged.rb
+++ lib/git-unmerged.rb
@@ -7,11 +7,11 @@ require 'term/ansicolor'
 
 class GitCommit
   attr_reader :content
-  
+
   def initialize(content)
     @content = content
   end
-  
+
   def sha
     @content.split[1]
   end
@@ -19,11 +19,11 @@ class GitCommit
   def to_s
     `git log --pretty=format:"%h %ad %an - %s" #{sha}~1..#{sha}`
   end
-  
+
   def unmerged?
     content =~ /^\+/
   end
-  
+
   def equivalent?
     content =~ /^\-/
   end
@@ -31,22 +31,22 @@ end
 
 class GitBranch
   attr_reader :name, :commits
-  
+
   def initialize(name, commits)
     @name = name
     @commits = commits
   end
-  
+
   # Returns origin from origin/some/branch/here
   def repository
     name.split("/", 2).first
   end
-  
+
   # Returns some/branch/here from origin/some/branch/here
   def branch_name
     name.split("/", 2).last
   end
-  
+
   def unmerged_commits
     commits.select{ |commit| commit.unmerged? }
   end
@@ -61,15 +61,15 @@ class GitBranches < Array
   def self.clean_branch_output(str)
     str.split(/\n/).map{ |e| e.strip.gsub(/\*\s+/, '') }.reject{ |branch| branch =~ /\b#{Regexp.escape(UPSTREAM)}\b/ }.sort
   end
-  
+
   def self.local_branches
     clean_branch_output `git branch`
   end
-  
+
   def self.remote_branches
     clean_branch_output `git branch -r`
   end
-  
+
   def self.load(options)
     git_branches = new
     branches = if options[:local]
@@ -77,18 +77,18 @@ class GitBranches < Array
     elsif options[:remote]
       remote_branches
     end
-    
+
     branches.each do |branch|
       raw_commits = `git cherry -v #{UPSTREAM} #{branch}`.split(/\n/).map{ |c| GitCommit.new(c) }
       git_branches << GitBranch.new(branch, raw_commits)
     end
     git_branches
   end
-  
+
   def unmerged
     reject{ |branch| branch.commits.empty? }.sort_by{ |branch| branch.name }
   end
-  
+
   def any_missing_commits?
     select{ |branch| branch.commits.any? }.any?
   end
@@ -96,22 +96,23 @@ end
 
 class GitUnmerged
   VERSION = "1.1"
-  
+
   include Term::ANSIColor
 
   attr_reader :branches
-  
+
   def initialize(args)
     @options = {}
     @branches_to_prune = []
     extract_options_from_args(args)
   end
-  
+
   def load
     @branches ||= GitBranches.load(:local => local?, :remote => remote?)
     @branches.reject!{|b| @options[:exclude].include?(b.name)} if @options[:exclude].is_a?(Array)
+    @branches.select!{|b| @options[:only].include?(b.name)} if @options[:only].is_a?(Array)
   end
-  
+
   def print_overview
     load
     if @options[:exclude] && @options[:exclude].length > 0
@@ -121,6 +122,15 @@ class GitUnmerged
       end
       puts
     end
+
+    if @options[:only] && @options[:only].length > 0
+      puts "The following branches will be compared against:"
+      @options[:only].each do |branch_name|
+        puts "  #{branch_name}"
+      end
+      puts
+    end
+
     if branches.any_missing_commits?
       puts "The following branches possibly have commits not merged to #{upstream}:"
       branches.each do |branch|
@@ -130,13 +140,13 @@ class GitUnmerged
       end
     end
   end
-  
+
   def print_help
     puts <<-EOT.gsub(/^\s+\|/, '')
       |Usage: #{$0} [-a] [--upstream <branch>] [--remote] [--prune]
       |
       |This script wraps the "git cherry" command. It reports the commits from all local
-      |branches which have not been merged into an upstream branch. 
+      |branches which have not been merged into an upstream branch.
       |
       |  #{yellow("yellow")} commits have not been merged
       |  #{green("green")} commits have equivalent changes in <upstream> but different SHAs
@@ -148,14 +158,15 @@ class GitUnmerged
       |  --remote (-r)  compare remote branches instead of local branches
       |  --upstream <branch>   specify a specific upstream branch (defaults to master)
       |  --exclude <branch>[,<branch>,...]   specify a comma-separated list of branches to exclude
+      |  --only <branch>[,<branch>,...]   specify a comma-separated list of branches to include
       |  --prune  prompts user to delete branches which have no differences with the upstream
-      | 
+      |
       |EXAMPLE: check for all unmerged commits
       |  #{$0}
       |
       |EXAMPLE: check for all unmerged commits and merged commits (but with a different SHA)
       |  #{$0} -a
-      | 
+      |
       |EXAMPLE: use a different upstream than master
       |  #{$0} --upstream otherbranch
       |
@@ -180,25 +191,25 @@ class GitUnmerged
     EOT
     exit
   end
-  
+
   def print_version
     puts "#{VERSION}"
   end
-    
+
   def branch_description
     local? ? "local" : "remote"
   end
-  
+
   def print_specifics
     load
-    
+
     if branches.any_missing_commits?
       print_breakdown
     else
       puts "There are no #{branch_description} branches out of sync with #{upstream}"
     end
   end
-  
+
   def print_breakdown
     puts "Below is a breakdown for each branch. Here's a legend:"
     puts
@@ -212,7 +223,7 @@ class GitUnmerged
           print ",", red(" this will be pruned")
           @branches_to_prune << branch
         end
-        print ")\n" 
+        print ")\n"
       else
         puts
       end
@@ -225,13 +236,13 @@ class GitUnmerged
       end
     end
   end
-  
+
   def print_legend
     load
     puts "  " + yellow("yellow") + " commits have not been merged"
     puts "  " + green("green") + " commits have equivalent changes in #{UPSTREAM} but different SHAs" if show_equivalent_commits?
   end
-  
+
   def prune
     return unless prune?
     if @branches_to_prune.empty?
@@ -250,7 +261,7 @@ class GitUnmerged
       if STDIN.gets=~/y/i
         @branches_to_prune.each do |branch|
           if local?
-            `git branch -D #{branch.name}` 
+            `git branch -D #{branch.name}`
           elsif remote?
             puts "pruning: #{branch.branch_name} with 'git push #{branch.repository} :#{branch.branch_name}'"
             `git push #{branch.repository} :#{branch.branch_name}`
@@ -262,7 +273,7 @@ class GitUnmerged
       end
     end
   end
-  
+
   def prune? ; @options[:prune ] ; end
   def show_help? ; @options[:show_help] ; end
   def show_equivalent_commits? ; @options[:show_equivalent_commits] ; end
@@ -277,16 +288,16 @@ class GitUnmerged
       "origin/master"
     end
   end
-  
+
   private
-  
+
   def extract_options_from_args(args)
     if args.include?("--remote") || args.include?("-r")
       @options[:remote] = true
     else
       @options[:local] = true
     end
-    
+
     @options[:prune] = true if args.include?("--prune")
     @options[:show_help] = true if args.include?("-h") || args.include?("--help")
     @options[:show_equivalent_commits] = true if args.include?("-a")
@@ -299,8 +310,12 @@ class GitUnmerged
     if index=args.index("--exclude")
       @options[:exclude] = args[index+1].split(',')
     end
+
+    if index=args.index("--only")
+      @options[:only] = args[index+1].split(',')
+    end
   end
-  
+
   def local? ; @options[:local] ; end
   def remote? ; @options[:remote] ; end
 end
